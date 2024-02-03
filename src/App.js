d7cfc2fe3c726934e217112ccacd90a0740553ce
myCodingSpace <88979248+Alfiyaali@users.noreply.github.com>
2024-02-03 17:43:13 +0530

    app files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, {useState} from 'react';
-import MyForm from './component/Users/MyForm';
-import UserList from './component/Users/UserList';
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [userList, setUserList] = useState([]);
+  const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) =>{
-    setUserList((prevUsersList) =>{
-      return [...prevUsersList, {name: uName, age: uAge, id:Math.random().toString() }];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
   return (
-     <div>
-      <MyForm onAddUser={addUserHandler} />
-      <UserList users={userList} />
-     </div>
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
   );
 }
 
